42aefa1a764d7ca6600bf9af89e67bb18923c18a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-09-02 23:21:51 +0900

    #2 express

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,10 @@
 import express, { Application } from "express";
-import serverlessExpress from "@vendia/serverless-express";
-import { Context } from "aws-lambda";
+import { v1Router } from "./presentation/rest/router";
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+app.use("/v1", v1Router());
 
-app.get("/api/ping", (req, res) => res.json({ message: "pong" }));
-
-export const handler = (event: any, context: Context, callback: any) => {
-    return serverlessExpress({ app })(event, context, callback);
-}
+export default app;
